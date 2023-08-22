386a6a29098f8daf09e4e21edf2b473a4ce530a4
Miguel Belo <belo_miguel@hotmail.com>
2023-08-22 15:24:42 +0100

    [Miguel] Added Bookings actions:  edit, new, create, update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :pets
+  resources :pets do
+    # Booking is displayed on /pets/:id/bookings/new
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:edit, :update]
 end
